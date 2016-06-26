9193c65708d07c1c6c2c152c11b78cce02cb9771
Radek Benes <radekbenes@centrum.cz>
2016-06-26 17:48:52 +0200

    26-06-16

---
+++ dev/weather/weather.data.ts
@@ -0,0 +1,6 @@
+import {WeatherItem} from './weather-item';
+
+export const WEATHER_ITEMS: WeatherItem[] = [
+    new WeatherItem('London', 'RAINY', 16),
+    new WeatherItem('Mlada Boleslav', 'SUNNY', 32)
+];
